6bec118d0d068ad1bd642233c26405d3a58bb9a2
Nate Snyder <n8.snyder@berkeley.edu>
2022-03-10 12:34:55 -0800

    Started adding user context

--- src/App.js
+++ src/App.js
@@ -2,14 +2,40 @@ import './App.css';
 import { BrowserRouter } from 'react-router-dom';
 import Routes from './Routes';
 import NavBar from './NavBar';
+import UserContext from "./userContext";
+import { useState } from 'react';
+import JoblyApi from './api';
+
 
 
 /** App root component
+ * 
+ * State:
+ * - user: object of the logged in user
+ * - token: string of authentication token used to make api calls
  * 
  * App -> {NavBar, Routes}
  */
 
 function App() {
+  const [user, setUser] = useState(null);
+  const [token, setToken] = useState(null);
+
+  async function signIn(userCredentials) {
+    const newToken = await JoblyApi.signIn(userCredentials);
+    setToken(newToken);
+  }
+
+  async function signUp(userData) {
+    const newToken = await JoblyApi.signUp(userData);
+    setToken(newToken);
+  }
+
+  async function update(userData) {
+    const updatedUserData = await JoblyApi.updateUser(userData);
+    setUser(updatedUserData);
+  }
+
   return (
     <div className="App">
       <BrowserRouter>
